303de3c30b769e984a33f80d008648b71c00957b
richardpho <rpho88@gmail.com>
2021-03-11 22:31:28 +0000

    update

--- script.js
+++ script.js
@@ -2,12 +2,3 @@
 $('.button').click(function () {
   $(this).parent().toggleClass('expand');     
 });
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
