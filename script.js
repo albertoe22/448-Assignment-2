c94c3a8f6582479b8a27d64059e5adf128e55162
Albert Truong <albertoe4788@gmail.com>
2021-03-11 22:22:17 +0000

    site changes

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 $('.button').click(function () {
   $(this).parent().toggleClass('expand');     
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
